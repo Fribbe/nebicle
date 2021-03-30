467702ffb031b8f3c982ed2a9d4c27a13c89a054
Duc Anh Nguyen <ducanh08@gmx.de>
2021-03-30 23:20:35 +0200

    added moon images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import './App.css';
+import moonimg from './images/moon-2021-03-26.jpg'
+import moonimg2 from './images/moon-2021-03-29.png'
+
 
 function App() {
   return (
     <div className="App">
-        Hallo
+      <div className="center">
+        <img src={moonimg} alt={'moon'}/>
+      </div>
     </div>
   );
 }
