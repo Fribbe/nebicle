75784e7c645cf22983da55b4f6a244ed703bdaa3
Duc Anh Nguyen <ducanh08@gmx.de>
2021-04-28 13:06:43 +0200

    added new moon image

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,11 +1,15 @@
-import moon1 from '../images/moon-2021-03-26.jpg';
-import moon2 from '../images/moon-2021-03-29.png';
+import moon1 from "../images/moon-2021-03-26.jpg";
+import moon2 from "../images/moon-2021-03-29.png";
+import moon3 from "../images/moon-2021-04-28.png";
 
 export const SliderData = [
-    {
-        image: moon1
-    },
-    {
-        image: moon2
-    }
-]
+	{
+		image: moon1,
+	},
+	{
+		image: moon2,
+	},
+	{
+		image: moon3,
+	},
+];
