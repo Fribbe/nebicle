fa9b17dee5bf0acc20316815544a5798c18f3d87
Duc Anh Nguyen <ducanh08@gmx.de>
2021-06-01 18:25:31 +0200

    added ngc 7000

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,8 +1,12 @@
 import moon1 from "../images/moon-2021-03-26.jpg";
 import moon2 from "../images/moon-2021-03-29.png";
 import moon3 from "../images/moon-2021-04-28.png";
+import ngc7000 from "../images/NGC7000-2021-06-01.jpg";
 
 export const SliderData = [
+	{
+		image: ngc7000,
+	},
 	{
 		image: moon1,
 	},
